ec65f11d8a1234278ec9501520841c492c659f29
wangshaotong <1277678521@qq.com>
2020-04-09 22:14:13 +0800

    枚举补充

--- acm/basic/enumerate/README.md
+++ acm/basic/enumerate/README.md
@@ -4,19 +4,20 @@
 
 ## 题目
 
-*PS:按难度排序*
+*按难度排序*
 
 - [poj 1003](http://poj.org/problem?id=1003) | [代码](./poj1003.cpp)
 - [hdu 1406](http://acm.hdu.edu.cn/showproblem.php?pid=1406) | [代码](./hdu1406.cpp)
 - [hdu 1692](http://acm.hdu.edu.cn/showproblem.php?pid=1692) | [代码](./hdu1692.cpp)
 - [poj 1006](http://poj.org/problem?id=1006) | [代码](./poj1006.cpp)
 - [hdu 1015](http://acm.hdu.edu.cn/showproblem.php?pid=1015) | [代码](./hdu1015.cpp)
+- [poj 1753](http://poj.org/problem?id=1753) | [代码](./poj1753.cpp)
 
 **TODO**:
 - ~~hdu 1692~~
 - hdu 4462
-- poj 1006
-- poj 1753
+- ~~poj 1006~~
+- ~~poj 1753~~
 - poj 1844
 - poj 2965
 - poj 3174
---
+++ acm/basic/enumerate/poj1753.cpp
@@ -0,0 +1,80 @@
+/**
+ * author : Wang Shaotong
+ * email  : wangshaotongsydemon@gmail.com
+ * date   : 2020-04-09
+ * 
+ * id     : poj 1753
+ * name   : Flip Game
+ * url    : http://poj.org/problem?id=1753
+ * level  : 2
+ * tag    : 枚举, 搜索, 位运算
+ */
+
+#include <iostream>
+using namespace std;
+
+const int SIDE = 4;
+const int INF = 0x3f3f3f;
+
+int chess[4][4];
+int cnt = 0, minCnt = INF;
+
+void flipIt(int x, int y) {
+    if (x >= 0 && x < 4 && y >= 0 && y < 4) {
+        chess[x][y] = !chess[x][y];
+    }
+}
+
+void flipThem(int x, int y) {
+    flipIt(x, y);
+    flipIt(x + 1, y);
+    flipIt(x - 1, y);
+    flipIt(x, y + 1);
+    flipIt(x, y - 1);
+}
+
+bool isSame() {
+    for (int i = 0; i < SIDE; i++) {
+        for (int j = 0; j < SIDE; j++) {
+            if (chess[i][j] != chess[0][0]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void dfs(int n) {
+    if (n == 16) {
+        if (isSame()) {
+            minCnt = cnt < minCnt ? cnt : minCnt;
+        } 
+        return;
+    }
+
+    for (int i = 0; i < 2; i++) {
+        if (i == 0) {
+            dfs(n + 1);
+        } else {
+            flipThem(n / SIDE, n % SIDE);
+            cnt++;
+            dfs(n + 1);
+            cnt--;
+            flipThem(n / SIDE, n % SIDE);
+        }
+    }
+}
+
+int main()
+{
+    for (int i = 0; i < SIDE; i++) {
+        string str;
+        cin >> str;
+        for (int j = 0; j < SIDE; j++) {
+            chess[i][j] = str[j] == 'b' ? 1 : 0;
+        }
+    }
+    dfs(0);
+    minCnt == INF ? cout << "Impossible" << endl : cout << minCnt << endl;
+    return 0;
+}
